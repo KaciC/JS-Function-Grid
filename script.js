291c7e9e94eb0bed19b1044cbd5d50ebbf53a998
Kaci Childers <kaci.childers@gmail.com>
2017-01-18 23:25:25 +0000

    pixels design

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //Write your function calls below this line
+function setColor(row, col, color) {
+    var cell = $('#t')[0].rows[row].cells[col];
+    cell.style.backgroundColor = color;
+}
+setColor(0, 0, "yellow");
+setColor(0, 1, "yellow");
+setColor(0, 2, "yellow");
+setColor(0, 3, "yellow");
+setColor(0, 4, "yellow");
+setColor(0, 5, "yellow");
+setColor(0, 6, "yellow");
+setColor(0, 7, "yellow");
+setColor(0, 8, "yellow");
+setColor(0, 9, "yellow");
+setColor(2, 2, "white");
+setColor(2, 1, "white");
